30a94b91b1e89b0926b339a0d7651d00cfb4df87
qinzhiwei1993 <384579070@qq.com>
2020-08-20 14:12:51 +0800

    add packcages

---
+++ packages/lerna-indepentend/packages/lerna-independent2/__tests__/lerna-independent2.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const lernaIndependent2 = require('..');
+
+describe('@qinzhiwei1993/lerna-independent2', () => {
+    it('needs tests');
+});
---
+++ packages/lerna-indepentend/packages/lerna-independent2/lib/lerna-independent2.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = lernaIndependent2;
+
+function lernaIndependent2() {
+    // TODO
+}
